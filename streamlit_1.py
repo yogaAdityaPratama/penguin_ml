7238426374a6506b0ac9815c1fb4dfa81b9cfb5e
yogaAdityaPratama <89354182+yogaAdityaPratama@users.noreply.github.com>
2023-07-14 03:13:09 +0700

    Update streamlit_1.py

--- streamlit_1.py
+++ streamlit_1.py
@@ -15,7 +15,7 @@ def pred(st):
     <style>
         [data-testid='stAppViewContainer']{
             font-style: roboto;
-            background-image: url(https://www.freepik.com/free-photos-vectors/penguins-carving-ice);
+            background-image: url(https://images4.alphacoders.com/206/206954.jpg);
             background-size: cover;
         }
         [data-testid='stSidebar']{
@@ -96,7 +96,7 @@ def viz(st):
     <style>
         [data-testid='stAppViewContainer']{
             font-style: roboto;
-            background-image: url(https://www.freepik.com/free-photos-vectors/penguins-carving-ice);
+            background-image: url(https://images4.alphacoders.com/206/206954.jpg);
             background-size: cover;
             
         }
