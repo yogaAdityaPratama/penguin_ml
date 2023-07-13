3343a41ff5ab0d7f6e4ce7470bd5eb0e9ecd0717
yogaAdityaPratama <89354182+yogaAdityaPratama@users.noreply.github.com>
2023-07-14 03:06:35 +0700

    Update streamlit_1.py

--- streamlit_1.py
+++ streamlit_1.py
@@ -15,7 +15,7 @@ def pred(st):
     <style>
         [data-testid='stAppViewContainer']{
             font-style: roboto;
-            background-image: url(https://images4.alphacoders.com/206/206954.jpg);
+            background-image: url(https://www.audubon.org/news/10-fun-facts-about-penguins);
             background-size: cover;
         }
         [data-testid='stSidebar']{
@@ -96,7 +96,7 @@ def viz(st):
     <style>
         [data-testid='stAppViewContainer']{
             font-style: roboto;
-            background-image: url(https://images4.alphacoders.com/206/206954.jpg);
+            background-image: url(https://www.audubon.org/news/10-fun-facts-about-penguins);
             background-size: cover;
             
         }
